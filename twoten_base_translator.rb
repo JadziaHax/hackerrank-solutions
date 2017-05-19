0a8e705549f6c68f6fea9b9c3da7724a46bfd217
JadziaHax <madelyn.a.thomas@gmail.com>
2017-05-19 10:04:02 -0700

    adds link to problem.

--- twoten_base_translator.rb
+++ twoten_base_translator.rb
@@ -3,8 +3,7 @@
 # base2 and find the longest succession of digit ones, and print the base10
 # version that base2 succession of digit ones represents to the command line.
 #
-# This solves the problem found at
-# https://www.hackerrank.com/challenges/30-binary-numbers
+# This solves the problem found at https://github.com/JadziaHax/hackerrank-solutions
 
 ##
 # This method takes a string expression of a base2 number. We specifiy two
@@ -61,29 +60,6 @@ def find_longest_successive_ones(string_number)
   build_base10(result)
 end
 
-##
-# Yes, as part of Ruby's method for turning integers into strings, .to_s, we can
-# pass an integer argument and have the resulting string representation of the
-# number expressed in whatever base we desire, but I felt that would be
-# antithetical to the point of the challenge, and less fun.
-#
-# This method takes a number in base 10, and has a default empty string
-# accumulator. The accumulator is only necessary so that we can call this
-# method recursively. I know I don't have to write a recursive method (a
-# while loop solution is rather obvious), but I happen to get warm fuzzies from
-# recursion, and will use it given the opportunity.
-#
-# First we divide the base10 by two and get both the quotient and the remainder.
-#
-# From there, we check to see if our recursive method's base case is true. In
-# this case we know that when our base10 number is divided by 2 and returns a
-# quotient of 0 with a remainder of 1, we're ready to make the remainder our
-# last addition to our base2 string and send that string to the method that will
-# find its longest count of consecutive digit ones.
-#
-# If we don't hit the base case, we add the remainder to our string accumulator
-# and pass it and the quotient into a recursive call of this same method.
-#
 def build_base2(base10_number, expression='')
 
   div_by_2 = base10_number.divmod(2)
